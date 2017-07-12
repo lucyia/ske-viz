6a07cf3cbf5c98b325e1eb1aa73b150700f99230
Lucia Kocincova <lucia.koc@gmail.com>
2017-07-13 00:32:14 +0100

    Updated demo

--- demo/index.js
+++ demo/index.js
@@ -3,13 +3,6 @@ import OppositeViz from 'opposite-viz/opposite-viz';
 
 const urlWSDiff = './data/wsdiff_house_home.json';
 const params = {
-  viz: {
-    divId: 'viz-container',
-    svgId: 'ske-viz-opposite',
-    className: 'wswiff-viz',
-    width: 800,
-    height: 500
-  },
   ticks: {
     show: true,
     number: 6
@@ -23,23 +16,60 @@ const params = {
   }
 };
 
+const showFirst = {
+  viz: {
+    divId: 'viz-container-1',
+    svgId: 'ske-viz-opposite-1',
+    className: 'wswiff-viz-1'
+  },
+  category: {
+    showItems: [1]
+  }
+};
+
+const showSecondThird = {
+  viz: {
+    divId: 'viz-container-2',
+    svgId: 'ske-viz-opposite-2',
+    className: 'wswiff-viz-2'
+  },
+  category: {
+    showItems: [2, 3]
+  }
+};
+
+const showAll = {
+  viz: {
+    divId: 'viz-container-3',
+    svgId: 'ske-viz-opposite-3',
+    className: 'wswiff-viz-3'
+  }
+};
+
+function catchError(event) {
+  // log the event
+  console.log(event);
+
+  // add the warning to the top of the window
+  const style = 'position:absolute; width:100%; height:30px; background:#000;'
+    + 'opacity:0.5; padding: 30px; color:white; font-size: 16px; text-align: center;';
+
+  // add explanation
+  const text = 'visualization could not be created, please check the console';
+
+  // text of the warning
+  const warning = `<div style="${style}">${event.type} - ${text}}</div>`;
+
+  document.body.innerHTML += warning;
+}
+
 parseURL(urlWSDiff, 'WS_DIFF')
   .then(data => {
-    OppositeViz(data, params);
+    // either show only a selected number
+    OppositeViz(data, Object.assign(showFirst, params));
+    OppositeViz(data, Object.assign(showSecondThird, params));
+
+    // or show all the categories
+    OppositeViz(data, Object.assign(showAll, params));
   })
-  .catch(event => {
-    // log the event
-    console.log(event);
-
-    // add the warning to the top of the window
-    const style = 'position:absolute; width:100%; height:30px; background:#000;'
-      + 'opacity:0.5; padding: 30px; color:white; font-size: 16px; text-align: center;';
-
-    // add explanation
-    const text = 'visualization could not be created, please check the console';
-
-    // text of the warning
-    const warning = `<div style="${style}">${event.type} - ${text}}</div>`;
-
-    document.body.innerHTML += warning;
-  });
+  .catch(catchError);
